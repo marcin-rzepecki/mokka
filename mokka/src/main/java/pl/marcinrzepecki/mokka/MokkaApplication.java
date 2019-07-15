039eb483d0264468f833743a6992bc6e6e3a6879
marcin-rzepecki <marcin.marek.rzepecki@gmail.com>
2019-07-15 21:33:50 +0200

    fix #1

---
+++ mokka/src/main/java/pl/marcinrzepecki/mokka/MokkaApplication.java
@@ -0,0 +1,13 @@
+package pl.marcinrzepecki.mokka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokkaApplication.class, args);
+	}
+
+}
